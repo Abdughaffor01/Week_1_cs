e265fd869618b85ec6cd409f7cc70174b3d599c5
Abdughaffor01 <138500396+Abdughaffor01@users.noreply.github.com>
2023-07-13 12:05:45 +0500

    day2_class

---
+++ day2_class/Person.cs
@@ -0,0 +1,19 @@
+public class Person
+{
+    public string Name;
+    public int Age;
+    public float Height;
+
+    public void FullInfo()
+    {
+        System.Console.WriteLine($"Name : {Name}  Age : {Age}  Height : {Height}");
+    }
+    public void Walk()
+    {
+        System.Console.WriteLine($"{Name} is walking");
+    }
+    public void Eat()
+    {
+        System.Console.WriteLine($"{Name} is eating");
+    }
+}
---
+++ day2_class/Program.cs
@@ -0,0 +1,8 @@
+﻿
+var person1 = new Person();
+person1.Name = "Idibek";
+person1.Age = 25;
+person1.Height = 1.90f;
+person1.FullInfo();
+person1.Walk();
+person1.Eat();
